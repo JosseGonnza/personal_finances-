2b6637a6fbffa3be381fd600108af10e3d93e200
JosseGonnza <accion.quevedo@hotmail.com>
2024-05-25 18:42:06 +0100

    First commit

---
+++ src/main/java/com/jossegonnza/finanzasPersonales/FinanzasPersonalesApplication.java
@@ -0,0 +1,14 @@
+package com.jossegonnza.finanzasPersonales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasPersonalesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FinanzasPersonalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jossegonnza/finanzasPersonales/FinanzasPersonalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jossegonnza.finanzasPersonales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasPersonalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
